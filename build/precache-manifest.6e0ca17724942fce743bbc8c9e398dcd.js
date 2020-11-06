1562b4c31d640e09769528f252ec52117e315073
martin-arreola <martin.arreola.dev@gmail.com>
2020-11-06 13:07:48 -0700

    added build

---
+++ build/precache-manifest.6e0ca17724942fce743bbc8c9e398dcd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62252d8485c2b59ef9f76fd365e50d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef31a7aedb4d83a2ca40",
+    "url": "/static/css/2.e69ac383.chunk.css"
+  },
+  {
+    "revision": "da90b2d362b6d6488298",
+    "url": "/static/css/main.119ff2bf.chunk.css"
+  },
+  {
+    "revision": "ef31a7aedb4d83a2ca40",
+    "url": "/static/js/2.5d934e26.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/static/js/2.5d934e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da90b2d362b6d6488298",
+    "url": "/static/js/main.e162b334.chunk.js"
+  },
+  {
+    "revision": "76cd7d063337681e2196",
+    "url": "/static/js/runtime-main.6dd20842.js"
+  },
+  {
+    "revision": "dc41cffd480f1ae92c57206e9ac265a9",
+    "url": "/static/media/fishsticks.dc41cffd.jpg"
+  }
+]);
